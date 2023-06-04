aaa47f84500a3fc2c6ec3a739dcb700964615201
Mario Diaz <dev.mariodiaz@gmail.com>
2023-06-04 15:47:25 +0200

    Initial commit and creating some initial classes

---
+++ NasaAPIConsumer.Services/NasaAPIService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAPIConsumer.Services
+{
+    public class NasaAPIService : INasaAPIService
+    {
+        private readonly HttpClient _httpClient;
+        public NasaAPIService(HttpClient httpClient)
+        {
+           _httpClient = httpClient;
+        }
+        public Task<List<INasaAPIService>> Get(int days)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
