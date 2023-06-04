43685db47fd648b7d3d20374aba12a6e47b6b6b0
Mario Diaz <dev.mariodiaz@gmail.com>
2023-06-05 01:09:47 +0200

    Getting data from retrieved json object

---
+++ NasaAPIConsumer.Domain/AsteroidResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAPIConsumer.Domain
+{
+    public  class AsteroidResponse
+    {
+        public string date { get; set; }
+        List<Asteroid> asteroids { get; set; }
+    }
+}
--- NasaAPIConsumer.Services/NasaAPIService.cs
+++ NasaAPIConsumer.Services/NasaAPIService.cs
@@ -23,6 +23,7 @@ namespace NasaAPIConsumer.Services
         public async Task Get(int days)
         {
             DateTime currentDate = DateTime.Now;
+            DateTime date = DateTime.Now;
             DateTime endDate = currentDate.AddDays(days);
             string url = $"{_configs.BaseUrl}?start_date={currentDate.ToString("yyyy-MM-dd")}&end_date={endDate.ToString("yyyy-MM-dd")}&API_KEY={_configs.ApiKey}";
 
@@ -32,9 +33,36 @@ namespace NasaAPIConsumer.Services
             {
                 string json = await response.Content.ReadAsStringAsync();
                 var jsonParse = JsonDocument.Parse(json);
-                var jsonFilter = JsonDocument.Parse(jsonParse.RootElement.GetProperty("near_earth_objects").ToString());
-                var asteroids = JsonSerializer.Deserialize<Asteroid>(jsonFilter);
+                var jsonFilter = jsonParse.RootElement.GetProperty("near_earth_objects").ToString();
+                var asteroids = JsonSerializer.Deserialize<JsonElement>(jsonFilter);
                 
+                while(date < endDate)
+                {
+                    var prop = asteroids.GetProperty(date.ToString("yyyy-MM-dd"));
+                    if(prop.ValueKind != JsonValueKind.Null && prop.ValueKind != JsonValueKind.Undefined)
+                    {
+                        for (int i = 0; i < prop.GetArrayLength(); i++)
+                        {
+                            var kilometers = prop[i].GetProperty("estimated_diameter").GetProperty("kilometers");
+                            var close_approach_data = prop[i].GetProperty("close_approach_data");
+
+                            var name = prop[i].GetProperty("name");
+                            var estimated_diameter_min = kilometers.GetProperty("estimated_diameter_min");
+                            var estimated_diameter_max = kilometers.GetProperty("estimated_diameter_max");                            
+                            var kilometers_per_hour = close_approach_data[0].GetProperty("relative_velocity").GetProperty("kilometers_per_hour");
+                            var close_approach_date = close_approach_data[0].GetProperty("close_approach_date");
+                            var orbiting_body = close_approach_data[0].GetProperty("orbiting_body");
+
+                            //if (bool.Parse(prop[i].GetProperty("is_potentially_hazardous_asteroid").ToString().ToLower()))
+                            //{
+
+                            //}
+                        }
+                    }                    
+
+                    date = date.AddDays(1);
+                }
+
             }
         }
     }
