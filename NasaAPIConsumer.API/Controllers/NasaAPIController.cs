68263c7c260f61568fe3dddea974c1a8eef2a863
Mario Diaz <dev.mariodiaz@gmail.com>
2023-06-05 08:10:40 +0200

    Adding restriction to days parameter between 1 and 7

--- NasaAPIConsumer.API/Controllers/NasaAPIController.cs
+++ NasaAPIConsumer.API/Controllers/NasaAPIController.cs
@@ -22,6 +22,8 @@ namespace NasaAPIConsumer.API.Controllers
             {
                 if (days == null)
                     return BadRequest("days parameter should be provided");
+                if (days > 0 && days <= 7)
+                    return BadRequest("days parameter should be between 1 and 7");
 
                 var response = await _nasaAPIService.Get(days);
                 return Ok(response);
